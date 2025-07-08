0194ee99619b3544a7c81c8a34d0e53a3a0df8ad
ALEKSANDR.I <alenamobilebkru@gmail.com>
2025-07-08 22:36:45 +0400

    first2

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
-$repo = 123;
+// $repo = 123;
 
-$repo += 456;
+// $repo += 456;
 
-echo"$repo";
+// echo"$repo";
+$goter = "hello, World!";
+// echo $goter; 
+// var_dump($repo);
+
+function hello($check) {
+echo "getra";
+
+return 8 + $check;
+}
+
+
+//echo hello(286);
+echo pow(3,8), ucfirst($goter);
+
+
